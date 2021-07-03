9a0eb94df8823ab7e861c7e680b77982e76bc62f
GreenGrasshopper28 <80503370+GreenGrasshopper28@users.noreply.github.com>
2021-07-03 13:09:27 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,29 @@
+/*var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;*/
+
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    const d;
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+   
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+  startTime();
+
